1d7d794db54285bcab7bc1536a7754b4343d7ed0
somrathumayan <somrathumayan@gmail.com>
2025-01-21 22:04:14 +0600

    create folder

---
+++ reference/fs_demo.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
+// create folder 
+
+fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+    if(err) throw err;
+    console.log("Folder Created...");
+});

12957536c32c5ca1605702cf14c4242340be27c8
somrathumayan <somrathumayan@gmail.com>
2025-01-21 22:10:31 +0600

    file written

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -3,7 +3,13 @@ const path = require("path");
 
 // create folder 
 
-fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+// fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+//     if(err) throw err;
+//     console.log("Folder Created...");
+// });
+
+// create file and write 
+fs.writeFile(path.join(__dirname, "/test", "Hello.txt"), "Hello Mr. Humayan", err => {
     if(err) throw err;
-    console.log("Folder Created...");
+    console.log("File written...");
 });

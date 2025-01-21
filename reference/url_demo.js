29e592f380511f62de900f954959cdf866d40d30
somrathumayan <somrathumayan@gmail.com>
2025-01-21 22:50:57 +0600

    add host and hostname

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -4,3 +4,9 @@ const myUrl = new URL("http://mywebsite.com/hello.html?id=100&status=active");
 
 // serialize URL
 console.log(myUrl.href);
+
+//host 
+console.log(myUrl.host);
+
+//hostname
+console.log(myUrl.hostname);

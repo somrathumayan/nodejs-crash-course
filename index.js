e7e0c864b17d3c3f5e58be96cf0cc7ccdb90cf1f
somrathumayan <somrathumayan@gmail.com>
2025-01-22 22:39:55 +0600

    read file from public

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end("<h1>Home</h1>")
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(content);
+        })
+
     }
 });
 

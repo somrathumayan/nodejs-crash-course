e6f67f522c574e321ec0c6babfdf7ee2bd737b7a
somrathumayan <somrathumayan@gmail.com>
2025-01-22 22:28:39 +0600

    nodemon setup for live change

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const person = require("./person");
 const per = new person("Md. Humayan Kabir", 27);
 
 per.greetings();
+
+
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.end("<h1>Home</h1>")
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
